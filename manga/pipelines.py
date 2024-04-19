ef25972d19001f3e76ba5084b8819921e857f391
Yobel D. <hlfeyexist@proton.me>
2024-04-19 21:53:39 +0700

    fix: remove ':' symbol from image

--- manga/pipelines.py
+++ manga/pipelines.py
@@ -19,7 +19,7 @@ class MangaInformationPipeline(ImagesPipeline):
     def file_path(self, request, response=None, info=None, *, item=None):
         item = MangaInformation(item)
         image_guid = hashlib.sha1(to_bytes(request.url)).hexdigest()
-        image_filename = f"manga_cover/{image_guid}{'_' + item['title'] if item['title'] != None else ''}.jpg"
+        image_filename = f"manga_cover/{image_guid}{'_' + item['title'].replace(':', '') if item['title'] != None else ''}.jpg"
 
         return image_filename
 
